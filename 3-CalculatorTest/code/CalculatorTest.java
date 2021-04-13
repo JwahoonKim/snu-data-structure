32cdb76770bc419471f68ac078eac0032b09c9ac
JwahoonKim <72662822+JwahoonKim@users.noreply.github.com>
2021-04-14 01:46:32 +0900

    CalculatorTest TestCase 성공ver

--- 3-CalculatorTest/code/CalculatorTest.java
+++ 3-CalculatorTest/code/CalculatorTest.java
@@ -18,11 +18,11 @@ public class CalculatorTest
 			}
 			catch (Exception e)
 			{
-				System.err.println("ERROR");
+				System.out.println("ERROR");
 			}
 		}
 	}
-	
+	//private임
 	private static void command(String input) throws Exception 
 	{
 		String answer = "";
@@ -43,7 +43,7 @@ public class CalculatorTest
 	}
 	
 	// 연산자들끼리 우선순위를 비교하기 위해 만든 함수
-	private static int priority(String operator) {
+	public static int priority(String operator) {
 		switch(operator) {
 		case "^": return 4;
 		case "~": return 3;
@@ -57,8 +57,24 @@ public class CalculatorTest
 		return -1;
 	}
 	
+	// 괄호가 쌍이 잘 맞는지 체크하는 함수 
+	public static boolean checkBracket(String input) {
+		int openBracket = 0;
+		
+		for(int i = 0; i < input.length(); i ++) {
+			char now = input.charAt(i);
+			if (now == '(') openBracket ++;
+			else if(now == ')'){
+				openBracket --;
+				if(openBracket < 0) return false;
+			}
+		}
+		if (openBracket != 0) return false;
+		return true;
+	}
+	
 	// 후위표기법을 만드는 규칙을 정의한 함수
-	private static void dealOperator(String operator, Stack<String> stack, List<String> list) {
+	public static void dealOperator(String operator, Stack<String> stack, List<String> list) {
 		if (stack.empty()) {
 			stack.push(operator);
 			return;
@@ -74,8 +90,8 @@ public class CalculatorTest
 				return;
 			}
 			else {
-				// ^가 두 번 이상 제곱꼴로 나올때는 기존 법칙과 좀 다르게 작동하여 조건문 추가
-				if(operator.equals("^") && last.equals("^")) {
+				// ^와 unary는 right-associative 라서 조건문 추가
+				if((operator.equals("^") && last.equals("^")) || (operator.equals("~") && last.equals("~"))) {
 					stack.push(operator);
 					return;
 				}
@@ -91,14 +107,58 @@ public class CalculatorTest
 	
 	// postfix를 List형태로 만드는 함수
 	public static List<String> makePostfix(String input) throws Exception {
+		String status = "";
+		boolean init = false;
+		int check = 0;
+		// 숫자-공백-숫자 순서대로 나온 경우 에러 발생
+		for(int i = 0; i < input.length(); i ++) {
+			char now = input.charAt(i);
+			if(!init && Character.isDigit(now)) {
+				init = true;
+				status = "Number";
+				check += 2;
+				continue;
+			}
+			if(init && Character.isDigit(now) && !status.equals("Number")) {
+				status = "Number";
+				check += 2;
+			}
+			else if(init && (now == ' ' || now == '\t') && !status.equals("Space")) {
+				status = "Space";
+				check += 1;
+			}
+			else if(init && !(now == ' ' || now == '\t') && !Character.isDigit(now)) {
+				status = "Operator";
+				check = 0;
+			}
+			
+			if(check >= 5) throw new Exception();
+		}
+		
 		//input의 공백 제거
 		input = input.replaceAll(" ", "").replaceAll("\t", "");
-		Stack<String> operStack = new Stack<>();
-		//ArrayList 사용 가능여부 질문
-		List<String> postfix = new ArrayList<>();
 		
+		// 괄호가 유효하지 않은 경우 에러 발생
+		if(checkBracket(input) == false) throw new Exception();
+		
+		//부호 뒤에 바로 닫는 괄호가 나오면 에러 발생
+		boolean isOperator = false;
+		for(int i = 0; i < input.length(); i ++) {
+			char now = input.charAt(i);
+			if(now == '*' || now == '/' || now == '%' || now == '+' || now == '-' || now == '^') {
+				isOperator = true;
+			}
+			else {
+				if(now == ')' && isOperator == true) throw new Exception();
+				else isOperator = false;
+			}
+		}
+		
+		Stack<String> operStack = new Stack<>();
+		List<String> postfix = new ArrayList<>();
 		String number = "";
 		boolean isUnary = true;
+		
 		for(int i = 0; i < input.length(); i++) {
 			String cur = Character.toString(input.charAt(i));
 			// cur가 숫자면 number에 추가
@@ -179,6 +239,7 @@ public class CalculatorTest
 	}
 	
 	public static boolean isOperator(String input) {
+		if(input.equals("-^")) return true;
 		switch(input) {
 		case "*": return true;
 		case "/": return true;
@@ -186,80 +247,70 @@ public class CalculatorTest
 		case "+": return true;
 		case "-": return true;
 		case "^": return true;
+		case "~": return true;
 		default: return false;
 		}
 	}
 	
 	// postfix를 List형태로 받아 이를 계산해주는 함수
 	public static long calculate(List<String> postfix) throws Exception {
-		long result = 0;
-		// postfix = [3, ~] 이라면 --> postfix = [-3] 으로 바꿔주는 기능 
-		while(true) {
-			int i = 0;
-			while(i < postfix.size()) {
-				if(postfix.get(i) == "~") {
-					String number = postfix.get(i - 1);
-					number = "-" + number;
-					postfix.remove(i - 1);
-					postfix.remove(i - 1);
-					postfix.add(i - 1, number);
-					postfix.add(i, " ");
-					break;
-				}
-				i++;
-			}
-			if(i == postfix.size()) break;
-		}
-		int count = 0;
-		for(int j = 0; j < postfix.size(); j ++) {
-			if(postfix.get(j).equals(" ")) count++;
-		}
-		for(int k = 0; k < count; k ++) {
-			postfix.remove(" ");
-		}
 		
-		int numCount = 0, operCount = 0;
-		for(int n = 0; n < postfix.size(); n++) {
-			if(isNumber(postfix.get(n))) numCount ++;
-			else operCount ++;
-		}
-		// postfix 안에 number 의 개수는 부호의 개수보다 1개 많아야 올바른 식임
-		if(numCount != operCount + 1) throw new Exception();
-		
-		// postfix의 원소를 하나씩 보면서 부호가 나오는 경우 후위표기식 계산법처럼 계산하는 과정
+		// postfix의 원소를 하나씩 보면서 부호가 나오는 경우 후위표기식 계산법에 따라 계산하는 과정
 		while(postfix.size() != 1) {
 			int cursor = 0;
 			while(!isOperator(postfix.get(cursor))) {
 				cursor ++;
 			}
-			long left = Long.parseLong(postfix.get(cursor - 2));
-			long right =  Long.parseLong(postfix.get(cursor - 1));
+						
+			String left = "";
+			if(cursor >= 2) left = postfix.get(cursor - 2);
+			String right = postfix.get(cursor - 1);
 			String operator = postfix.get(cursor);
-			switch(operator) {
-			case "*": 
-				result = left * right;
-				break;
-			case "/": 
-				result = left / right;
-				break;
-			case "%": 
-				result = left % right;
-				break;
-			case "+": 
-				result = left + right;
-				break;
-			case "-": 
-				result = left - right;
-				break;
-			case "^":
-				if(right < 0) throw new Exception();
-				result = (long)Math.pow(left, right);
-				break;
+			
+			if(operator.equals("~")) {
+				if (right.charAt(0) == '-') {
+					right = right.substring(1);
+				}
+				else right = "-" + right;
+				
+				postfix.remove(cursor - 1);
+				postfix.remove(cursor - 1);
+				postfix.add(cursor - 1, right);
 			}
+			
+			else {
+				long result = 0;
+				long leftL = Long.parseLong(left);
+				long rightL = Long.parseLong(right);
+			
+				switch(operator) {
+				case "*": 
+					result = leftL * rightL;
+					break;
+				case "/": 
+					result = leftL / rightL;
+					break;
+				case "%": 
+					result = leftL % rightL;
+					break;
+				case "+": 
+					result = leftL + rightL;
+					break;
+				case "-": 
+					result = leftL - rightL;
+					break;
+				case "^":
+					if(leftL == 0 && rightL < 0) throw new Exception();
+					result = (long)Math.pow(leftL, rightL);
+					break;
+				default: throw new Exception();
+			}
+			
 			postfix.remove(cursor - 2);
 			postfix.remove(cursor - 2);
 			postfix.remove(cursor - 2);
 			postfix.add(cursor - 2, Long.toString(result));
+			}
 		}
 		return Long.parseLong(postfix.get(0));
 	}
